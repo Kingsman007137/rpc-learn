bfcabb3558ef60e2fdb5f94d26907faed44764ba
pan <3398530682@qq.com>
2024-08-12 11:49:49 +0800

    完成提供者处理调用——请求处理器

---
+++ rpc-easy/src/main/java/com/kingsman/rpc/server/HttpServerHandler.java
@@ -0,0 +1,84 @@
+package com.kingsman.rpc.server;
+
+import com.kingsman.rpc.model.RpcRequest;
+import com.kingsman.rpc.model.RpcResponse;
+import com.kingsman.rpc.registry.LocalRegistry;
+import com.kingsman.rpc.serializer.JdkSerializer;
+import com.kingsman.rpc.serializer.Serializer;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.buffer.Buffer;;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+/**
+ * HTTP 请求处理器
+ */
+@Slf4j
+public class HttpServerHandler implements Handler<HttpServerRequest> {
+    @Override
+    public void handle(HttpServerRequest request) {
+        final Serializer serializer = new JdkSerializer();
+        log.info("Received request: " + request.method() + " " + request.uri());
+
+        // 异步处理 HTTP 请求
+        request.bodyHandler(body -> {
+            byte[] bytes = body.getBytes();
+            RpcRequest rpcRequest = null;
+            try {
+                // 反序列化请求
+                rpcRequest = serializer.deserialize(bytes, RpcRequest.class);
+            } catch (Exception e) {
+                log.error("Failed to deserialize request", e);
+                e.printStackTrace();
+            }
+
+            // 构造响应结果对象
+            RpcResponse rpcResponse = new RpcResponse();
+            // 如果请求为空，返回错误信息
+            if (rpcRequest == null) {
+                rpcResponse.setMessage("Request is null");
+                doResponse(request, rpcResponse, serializer);
+                return;
+            }
+
+            // 调用服务提供者的方法
+            try {
+                Class<?> implClass = LocalRegistry.get(rpcRequest.getServiceName());
+                Method method = implClass.getMethod(rpcRequest.getMethodName(), rpcRequest.getParameterTypes());
+                Object result = method.invoke(implClass.newInstance(), rpcRequest.getArgs());
+                // 封装调用结果
+                rpcResponse.setData(result);
+                rpcResponse.setDataType(method.getReturnType());
+                rpcResponse.setMessage("Success");
+            } catch (Exception e) {
+                e.printStackTrace();
+                rpcResponse.setMessage(e.getMessage());
+                rpcResponse.setException(e);
+            }
+        });
+    }
+
+    /**
+     * 处理响应
+     *
+     * @param request
+     * @param rpcResponse
+     * @param serializer
+     */
+    private void doResponse(HttpServerRequest request, RpcResponse rpcResponse, Serializer serializer) {
+        HttpServerResponse httpServerResponse = request.response()
+                .putHeader("content-type", "application/json");
+        try {
+            // 序列化
+            byte[] serialized = serializer.serialize(rpcResponse);
+            httpServerResponse.end(Buffer.buffer(serialized));
+        } catch (IOException e) {
+            e.printStackTrace();
+            httpServerResponse.end(Buffer.buffer());
+        }
+    }
+}
