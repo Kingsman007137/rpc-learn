7669b2d230e7d62f0b2c5f56c2a062fdf6538f78
pan <3398530682@qq.com>
2024-08-13 22:32:54 +0800

    修改记录日志方式

--- rpc-easy/src/main/java/com/kingsman/rpc/server/HttpServerHandler.java
+++ rpc-easy/src/main/java/com/kingsman/rpc/server/HttpServerHandler.java
@@ -8,8 +8,7 @@ import com.kingsman.rpc.serializer.Serializer;
 import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.buffer.Buffer;;
-import lombok.extern.slf4j.Slf4j;
+import io.vertx.core.buffer.Buffer;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
@@ -17,12 +16,12 @@ import java.lang.reflect.Method;
 /**
  * HTTP 请求处理器
  */
-@Slf4j
 public class HttpServerHandler implements Handler<HttpServerRequest> {
     @Override
     public void handle(HttpServerRequest request) {
         final Serializer serializer = new JdkSerializer();
-        log.info("Received request: " + request.method() + " " + request.uri());
+        // 记录日志
+        System.out.println("Received request: " + request.method() + " " + request.uri());
 
         // 异步处理 HTTP 请求
         request.bodyHandler(body -> {
@@ -32,7 +31,6 @@ public class HttpServerHandler implements Handler<HttpServerRequest> {
                 // 反序列化请求
                 rpcRequest = serializer.deserialize(bytes, RpcRequest.class);
             } catch (Exception e) {
-                log.error("Failed to deserialize request", e);
                 e.printStackTrace();
             }
 
